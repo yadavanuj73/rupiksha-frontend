4b915300d63891dbaa4b89eeec25113d8211c3b8
yadavanuj73 <yadavanuj2068@gmail.com>
2026-06-30 13:56:25 +0530

    Fix AEPS Spring bean scan and repository name collision

--- backend-java/src/main/java/com/rupiksha/aeps/repository/AepsUserRepository.java
+++ backend-java/src/main/java/com/rupiksha/aeps/repository/AepsUserRepository.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository("aepsUserRepository")
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface AepsUserRepository extends JpaRepository<User, Long> {
     Optional<User> findByMobile(String mobile);
     Optional<User> findByUsername(String username);
 }
